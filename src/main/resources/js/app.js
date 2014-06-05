2787e065bffafba08da4772a83d1ba9fed29efad
Ian Lim <mallim.ink@gmail.com>
2014-06-05 23:38:45 +0800

    Finished Edit Profile, Change Password,

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,8 +1,12 @@
 require("angular");
 require("angular-route");
-require('angular-strap');
+require('angular-bootstrap');
 
-var app = angular.module('sbangularApp', ['ngRoute','mgcrea.ngStrap']);
+/**
+ * ui.bootstrap is from angular-bootstrap
+ * It is being use to do the accordion for the Address
+ */
+var app = angular.module('sbangularApp', ['ngRoute','ui.bootstrap']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
@@ -16,9 +20,37 @@ app.config(['$routeProvider', function($routeProvider){
             templateUrl:'home.tpl',
             controller:null
         })
+        .when('/userform',{
+            title:'User Settings',
+            templateUrl:'userSettings.tpl',
+            controller:"UserSettingsCtrl"
+        })
+        .when('/updatePassword',{
+            title:'Update Your Password',
+            templateUrl:'updatePassword.tpl',
+            controller:null
+        })
+        .when('/logout',{
+          title:'Login',
+          templateUrl:'login.tpl',
+          controller:null
+        })
+        .when('/signup',{
+          title:'Sign Up',
+          templateUrl:'signup.tpl',
+          controller:null
+        })
         .otherwise({
             redirectTo: '/login'
         });
+}]);
+
+app.controller('UserSettingsCtrl', ['$scope',function($scope){
+    $scope.oneAtATime = true;
+    $scope.status = {
+        isFirstOpen: false,
+        isFirstDisabled: false
+    };
 }]);
 
 app.factory('messageService',[ '$rootScope', function ($rootScope) {
@@ -67,13 +99,14 @@ app.controller('LoginCtrl', ['$scope', '$location', 'messageService',
 
         messageService.notifyLogin("successfully login", "LoginCtrl");
 
-        console.log("Going to redirect to /home");
-
         $location.path( "/home" );
     }
 
 }]);
 
+/**
+ * This part will change the html's header title
+ */
 app.run(['$location', '$rootScope', function($location, $rootScope) {
     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
         $rootScope.title = current.$$route.title;

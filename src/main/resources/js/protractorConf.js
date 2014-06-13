2260049e8cbb64c5b38efa772bb98afa3748f538
Ian Lim <mallim.ink@gmail.com>
2014-06-13 20:42:14 +0800

    Travis CI  -  use firefox driver instead

--- src/main/resources/js/protractorConf.js
+++ src/main/resources/js/protractorConf.js
@@ -7,7 +7,7 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
 
   framework: 'mocha',
